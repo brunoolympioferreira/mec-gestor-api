25577c1b33cf7474880a415f07c165643aa50db7
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-11-15 13:36:36 -0300

    feat: configura MexGestorDbContext

---
+++ MecGestor/MecGestor.Infra/Persistence/MecGestorDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MecGestor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace MecGestor.Infra.Persistence;
+
+public class MecGestorDbContext(DbContextOptions<MecGestorDbContext> options) : DbContext(options)
+{
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Plan> Plans { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
