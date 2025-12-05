269e4590b7d197b3de27b3d14bb3d968727a131d
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-12-05 18:44:50 -0300

    feat: implementa model de request para user

---
+++ MecGestor/MecGestor.Application/Models/Requests/CreateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using MecGestor.Domain.Entities;
+
+namespace MecGestor.Application.Models.Requests;
+
+public record CreateUserRequest(string Username, string Email, string Password, string Role, bool Active, Guid CompanyId)
+{
+    public User ToEntity()
+    {
+        return new User(Username, Email, Password, Role, Active, CompanyId);
+    }
+}
